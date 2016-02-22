91f7648f463a0c9210cafe7957fdfaa7d85b81b9
Chino Kim <chino@spotify.com>
2016-02-22 16:49:58 -0500

    recalibrate sensor values

--- power/power.ino
+++ power/power.ino
@@ -1,5 +1,5 @@
 int started = 0;
-bool on0 = false;
+bool power = false;
 bool on1 = false;
 bool on2 = false;
 bool on3 = false;
@@ -10,6 +10,8 @@ bool on7 = false;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(12, OUTPUT);
+  pinMode(13, INPUT);
 }
 
 void loop() {
@@ -20,135 +22,154 @@ void loop() {
   
   if (started) {
     int s0 = digitalRead(13);
-    int s1 = analogRead(A1);
-    int s2 = analogRead(A2);
-    int s3 = analogRead(A3);
-    int s4 = analogRead(A4);
-    int s5 = analogRead(A5);
-    int s6 = analogRead(A6);
-    int s7 = analogRead(A7);
+    int s1 = analogRead(A1); // 15
+    int s2 = analogRead(A2); // 10
+    int s3 = analogRead(A3); // 20
+    int s4 = analogRead(A4); // 10
+    int s5 = analogRead(A5); // 15
+    int s6 = analogRead(A6); // 15
+    int s7 = analogRead(A7); // 20
 
-//    Serial.println(s1);
+//    Serial.println(s6);
 
     if (!s0) {
-      if (on0) {
+      if (power) {
         Serial.print('0');
         Serial.print(' ');
         Serial.println('0');
-        on0 = false;
+        digitalWrite(12, LOW);
+        power = false;
       }
     } else {
-      if (!on0) {
-        Serial.print('0');
+      if (!power) {
+        Serial.print('1');
         Serial.print(' ');
-        Serial.println('1');
-        on0 = true;
+        Serial.println('0');
+        digitalWrite(12, HIGH);
+        power = true;
       }
     };
-    if (s1 < 100) {
+
+    if (power) {
       if (on1) {
-        Serial.print('1');
-        Serial.print(' ');
-        Serial.println('0');
-        on1 = false;
-      }
-    } else {
-      if (!on1) {
-        Serial.print('1');
-        Serial.print(' ');
-        Serial.println('1');
-        on1 = true;
-      }
-    };
-    if (s2 < 100) {
-      if (on2) {
-        Serial.print('2');
-        Serial.print(' ');
-        Serial.println('0');
-        on2 = false;
-      }
-    } else {
-      if (!on2) {
-        Serial.print('2');
-        Serial.print(' ');
-        Serial.println('1');
-        on2 = true;
-      }
-    };
-    if (s3 < 100) {
+        if (s1 < 100) {
+          Serial.print('0');
+          Serial.print(' ');
+          Serial.println('1');
+          delay(10);
+          on1 = false;
+        }
+      } else {
+        if (s1 > 100) {
+          Serial.print('1');
+          Serial.print(' ');
+          Serial.println('1');
+          delay(10);
+          on1 = true;
+        }
+      };
+      if (on2) {  
+        if (s2 < 100) {
+          Serial.print('0');
+          Serial.print(' ');
+          Serial.println('2');
+          delay(10);
+          on2 = false;
+        }
+      } else {
+        if (s2 > 100) {
+          Serial.print('1');
+          Serial.print(' ');
+          Serial.println('2');
+          delay(10);
+          on2 = true;
+        }
+      };
       if (on3) {
-        Serial.print('3');
-        Serial.print(' ');
-        Serial.println('0');
-        on3 = false;
-      }
-    } else {
-      if (!on3) {
-        Serial.print('3');
-        Serial.print(' ');
-        Serial.println('1');
-        on3 = true;
-      }
-    };
-    if (s4 < 100) {
+        if (s3 < 100) {
+          Serial.print('0');
+          Serial.print(' ');
+          Serial.println('3');
+          delay(10);
+          on3 = false;
+        }
+      } else {
+        if (s3 > 100) {
+          Serial.print('1');
+          Serial.print(' ');
+          Serial.println('3');
+          delay(10);
+          on3 = true;
+        }
+      };
       if (on4) {
-        Serial.print('4');
-        Serial.print(' ');
-        Serial.println('0');
-        on4 = false;
-      }
-    } else {
-      if (!on4) {
-        Serial.print('4');
-        Serial.print(' ');
-        Serial.println('1');
-        on4 = true;
-      }
-    };
-    if (s5 < 100) {
+        if (s4 < 100) {
+          Serial.print('0');
+          Serial.print(' ');
+          Serial.println('4');
+          delay(10);
+          on4 = false;
+        }
+      } else {
+        if (s4 > 100) {
+          Serial.print('1');
+          Serial.print(' ');
+          Serial.println('4');
+          delay(10);
+          on4 = true;
+        }
+      };
       if (on5) {
-        Serial.print('5');
-        Serial.print(' ');
-        Serial.println('0');
-        on5 = false;
-      }
-    } else {
-      if (!on5) {
-        Serial.print('5');
-        Serial.print(' ');
-        Serial.println('1');
-        on5 = true;
-      }
-    };
-    if (s6 < 100) {
+        if (s5 < 100) {
+          Serial.print('0');
+          Serial.print(' ');
+          Serial.println('5');
+          delay(10);
+          on5 = false;
+        }
+      } else {
+        if (s5 > 100) {
+          Serial.print('1');
+          Serial.print(' ');
+          Serial.println('5');
+          delay(10);
+          on5 = true;
+        }
+      };
       if (on6) {
-        Serial.print('6');
-        Serial.print(' ');
-        Serial.println('0');
-        on6 = false;
-      }
-    } else {
-      if (!on6) {
-        Serial.print('6');
-        Serial.print(' ');
-        Serial.println('1');
-        on6 = true;
-      }
-    };
-    if (s7 < 100) {
+        if (s6 < 50) {
+          Serial.print('0');
+          Serial.print(' ');
+          Serial.println('6');
+          delay(10);
+          on6 = false;
+        }
+      } else {
+        if (s6 > 50) {
+          Serial.print('1');
+          Serial.print(' ');
+          Serial.println('6');
+          delay(10);
+          on6 = true;
+        }
+      };
       if (on7) {
-        Serial.print('7');
-        Serial.print(' ');
-        Serial.println('0');
-        on7 = false;
-      }
-    } else {
-      if (!on7) {
-      Serial.print('7');
-      Serial.print(' ');
-      Serial.println('1');
-      on7 = true;
-      }
+        if (s7 < 100) {
+          Serial.print('0');
+          Serial.print(' ');
+          Serial.println('7');
+          delay(10);
+          on7 = false;
+        }
+      } else {
+        if (s7 > 100) {
+          Serial.print('1');
+          Serial.print(' ');
+          Serial.println('7');
+          delay(10);
+          on7 = true;
+        }
+      };
     };
-  }
+  };
 }
